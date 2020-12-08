f3e04a9af8496af98d1402d1908215cd7c76865f
psgoral <icegoral@gmail.com>
2020-12-08 15:03:19 +0100

    day3 done

--- Day3/day3.py
+++ Day3/day3.py
@@ -1,44 +1,51 @@
 def main():
 
-    input_map = [x.strip() for x in open('input.txt','r').readlines()]
+    # input_map = [x.strip() for x in open('gracjan.txt','r').readlines()]
+    # new_map = []
+    # for line in input_map:
+    #     new_map.append(str(line) * 100)
 
-    new_map = []
+    #ONELINER
+    new_map = [line*100 for line in [x.strip() for x in open('input.txt','r').readlines()]]
 
 
-    for line in input_map:
-        new_map.append(str(line) * 100)
+    # fd = open('mapa.txt','w+')
     
+    # for line in new_map:
+    #     fd.write(str(line) + '\n')
 
-
-    print()
     #PART 1
     pos = [0,0]
     move = [1,3]
     counter = 0
-    while pos[0] < len(new_map) and pos[1] < len(new_map[0]):
-        if new_map[pos[0]][pos[1]] == '#':
-            counter +=1
+    
+    while pos[0] < len(new_map):
+        if pos[0] != 0 and pos[1] != 0:
+            if new_map[pos[0]][pos[1]] == '#':
+                counter +=1
         pos[0] += move[0]
         pos[1] += move[1]
         
     print(str(counter) + ' trees')
 
-
     #PART 2
-
-    slopes = [[1,1],[1,3],[1,5],[1,7],[1,9],[2,1]]
+    slopes = [[1,1],[1,3],[1,5],[1,7],[2,1]]
 
     answers = []
     answer = 1
     for slope in slopes:
         jumps = 0
         pos = [0,0]
+        pos[0] += slope[0]
+        pos[1] += slope[1]
         counter = 0
         while pos[0] < len(new_map):
-            if new_map[pos[0]][pos[1]] == '#':
-                counter +=1
+            if pos[0] != 0:
+                if new_map[pos[0]][pos[1]] == '#':
+                    counter +=1
             pos[0] += slope[0]
             pos[1] += slope[1]
+            # print(pos)
             jumps += 1
         print(str(slope) + ':\t' + str(jumps) + ' jumps,\t' + str(counter) + ' trees')
         answers.append(counter)
