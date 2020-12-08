249c265a46e64bb386957b7cfb8c38fa6388b569
psgoral <icegoral@gmail.com>
2020-12-08 06:22:14 +0100

    day1 done

--- Day1/day1.py
+++ Day1/day1.py
@@ -5,7 +5,26 @@
 
 def main():
 
-    pass
+    numbers = [int(x) for x in open('input.txt','r').readlines()]
+
+
+
+    #PART1
+    for num1 in numbers:
+        for num2 in numbers:
+            if num1 + num2 == 2020 and num1 != num2:
+                print(str(num1) + ' * ' + str(num2) + ' = '+ str(int(num1) * int(num2)))
+
+    #PART2
+
+    for num1 in numbers:
+        for num2 in numbers:
+            for num3 in numbers:
+                if num1 + num2 + num3 == 2020 and (num1 != num2 != num3):
+                    print(str(num1) + ' * ' + str(num2) + ' * ' + str(num3) + ' = '+ str(num1 * num2 * num3))
+
+
+
 
 if __name__ == '__main__':
     main()
---
+++ Day2/day2.py
@@ -0,0 +1,13 @@
+
+
+
+
+
+def main():
+
+    numbers = [int(x) for x in open('input.txt','r').readlines()]
+
+
+
+if __name__ == '__main__':
+    main()
