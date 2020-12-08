b9b02e85f68b00c4882c4a8916a459f0284bfaab
psgoral <icegoral@gmail.com>
2020-12-08 06:51:03 +0100

    day2 done

--- Day2/day2.py
+++ Day2/day2.py
@@ -3,9 +3,57 @@
 
 
 
+
+def letterCounter(letter,password):
+    sum = 0
+    for l in password:
+        if letter == l:
+            sum +=1
+
+    return sum
+
 def main():
 
-    numbers = [int(x) for x in open('input.txt','r').readlines()]
+    input_passwords = [x.replace('\n', '') for x in open('input.txt','r').readlines()]
+
+
+    #1-4 m: mrfmmbjxr
+
+
+    #PART1
+
+    valid = 0
+    for pw in input_passwords:
+        password = pw.split(' ')
+        
+        minmax = [int(x) for x in password[0].split('-')]
+        letter = password[1].replace(':', '')
+
+        if letterCounter(letter,password[2]) >= minmax[0] and letterCounter(letter,password[2]) <= minmax[1]:
+            valid +=1
+
+    print(str(valid) + ' passwords')
+
+
+    #PART2
+
+    valid = 0
+    for pw in input_passwords:
+        password = pw.split(' ')
+        
+        places = [int(x)-1 for x in password[0].split('-')]
+        letter = password[1].replace(':', '')
+        print(password[2])
+        print(password[2][places[0]:places[0]+1])
+
+        if (letter == password[2][places[0]:places[0]+1] and letter != password[2][places[1]:places[1]+1]) or (letter != password[2][places[0]:places[0]+1] and letter == password[2][places[1]:places[1]+1]):
+            valid +=1
+
+    print(str(valid) + ' passwords')
+
+
+
+
 
 
 
